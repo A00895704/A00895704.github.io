c1b78e1fbd5ba9efa02f2ff5028874c5e91022fa
Erik H <erik.624.h@gmail.com>
2020-02-04 20:49:21 -0800

    add lab3

---
+++ js/lab3.js
@@ -0,0 +1,39 @@
+function add(elem){
+    var addForm = document.getElementById("addForm");
+   if(addForm.style.display != "none"){
+     addForm.style.display = "none";
+   } else{
+     addForm.style.display="block";
+   }
+   // document.getElementById("add").style.display="none";
+   //alert("Yes you clicked it");
+ 
+ }
+ 
+ function addArtistToCollection(elem){
+   var addForm = document.getElementById("addForm");
+   addForm.style.display="none";
+   var artistList = document.getElementById("artistList");  
+   var artistItem = document.createElement("li");
+   
+   var artistPicture = document.createElement("img");
+   var artistURL = document.getElementById("addURL");
+    
+   var divDesc = document.createElement("div");
+   var pName = document.createElement("p");
+   var pAbout = document.createElement("p");
+   var artistName = document.getElementById("addName");
+   var artistAbout = document.getElementById("addAbout");
+   pName.textContent = artistName.Value;
+   pAbout.textContent = artistAbout.Value;
+   
+   divDesc.appendChild(pName);
+   divDesc.appendChild(pAbout);
+   
+   artistPicture.setAttribute("src", artistURL.Value);
+   artistList.appendChild(artistItem);
+   artistList.appendChild(divDesc);
+   
+  
+   
+ }
